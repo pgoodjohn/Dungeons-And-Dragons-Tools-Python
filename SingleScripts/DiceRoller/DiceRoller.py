a2872d396a48ec5a8260c9be54fe9d79b67f69c7
erlafont <eric.lafontaine1@gmail.com>
2016-09-02 18:02:32 -0400

    updated gitignore + python 3 on dice roller

--- SingleScripts/DiceRoller/DiceRoller.py
+++ SingleScripts/DiceRoller/DiceRoller.py
@@ -12,10 +12,10 @@ while True:
     diceNumber = input('Choose number of dices ')
     diceType = input('Choose dice type ')
     dice = 0
-    diceTotal = 0;
+    diceTotal = 0
 
-    for dice in range(0, diceNumber):
-        diceRoll = randint(1, diceType)
+    for dice in range(0, int(diceNumber)):
+        diceRoll = randint(1, int(diceType))
         diceTotal = diceTotal + diceRoll
-        print "Dice #%r = %r" % (dice+1, diceRoll)
-    print "Result is %r" % diceTotal
+        print("Dice #%r = %r" % (dice+1, diceRoll))
+    print("Result is %r" % diceTotal)
